69c87bb16e242c74ad9946720330bce551621f5a
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-03 12:07:02 +0530

    create Form.jsx

---
+++ src/Components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../index.css"
+export function Form() {
+  return (
+    <>
+        <div className='add-form'>
+            <h3>What do you need for your 🎒 trip?</h3>
+        </div>
+    
+    </>
+  )
+}
+
