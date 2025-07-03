6f2102c145b3cef8a8110c37bea7402098fd0e3c
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-03 16:54:40 +0530

    handle form submitting

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import "../index.css"
 export function Form() {
-  return (
-    <>
-        <form className='add-form'>
-            <h3>What do you need for your 🎒 trip?</h3>
-            <select>
-                {Array.from({length : 10}, ( _, i ) => i + 1).map
-                ((num) => 
-                    <option value={num} key={num}>
-                        {num}
-                    </option>    
-                )}
-            </select>
-            <input type='text' placeholder='Item...'/>
-            <button>Add</button>
-        </form>
-    
-    </>
-  )
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        
+    }
+    return (
+        <>
+            <form className='add-form' onSubmit={handleSubmit}>
+                <h3>What do you need for your 🎒 trip?</h3>
+                <select>
+                    {Array.from({ length: 10 }, (_, i) => i + 1).map
+                        ((num) =>
+                            <option value={num} key={num}>
+                                {num}
+                            </option>
+                        )}
+                </select>
+                <input type='text' placeholder='Item...' />
+                <button>Add</button>
+            </form>
+
+        </>
+    )
 }
 
