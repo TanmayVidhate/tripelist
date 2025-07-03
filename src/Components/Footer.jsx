2ffd951c07e8f12dc1cf847a681ba777d3a334ea
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-03 12:22:11 +0530

    create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function Footer() {
+  return (
+    <footer className='stats'>
+        <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  )
+}
+
