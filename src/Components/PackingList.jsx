5630814600b5bf93f0405013b946b06e24857f4d
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-04 17:31:28 +0530

    add delete item feature

--- src/Components/PackingList.jsx
+++ src/Components/PackingList.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import "../index.css"
 // import { initialItems } from '../../public/data'
 import Listitem from './Listitem'
-export function PackingList({newItem}) {
+export function PackingList({newItem, ondelete}) {
     return (
         <>
             <div className='list'>
                 <ul>
                     {
                         newItem.map((item, id) =>
-                            <Listitem item={item} key={id} />
+                            <Listitem item={item} key={id} ondelete = {ondelete} />
                         )
                     }
                 </ul>
